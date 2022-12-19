5d08426383b02c59ade0e5641ca8dc73f8563179
reeruryu <h2ju1004@gmail.com>
2022-12-19 15:59:53 +0900

    프로젝트생성

---
+++ src/main/java/com/example/smartorder/SmartorderApplication.java
@@ -0,0 +1,13 @@
+package com.example.smartorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartorderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartorderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/smartorder/SmartorderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.smartorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartorderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
