7907b108e0a8ca99ea589c285eef3d08af21f7ab
reeruryu <h2ju1004@gmail.com>
2023-01-22 00:21:20 +0900

    [fix] @Value로 apiKey 값 변경

--- src/main/java/com/example/smartorder/common/component/LocationComponents.java
+++ src/main/java/com/example/smartorder/common/component/LocationComponents.java
@@ -3,43 +3,28 @@ package com.example.smartorder.common.component;
 import static com.example.smartorder.common.error.ErrorCode.FAIL_CALL_KAKAO_API;
 import static com.example.smartorder.common.error.ErrorCode.INVALID_ADDRESS;
 import static org.springframework.http.HttpHeaders.AUTHORIZATION;
-import static org.springframework.http.MediaType.*;
 
 import com.example.smartorder.common.exception.CustomException;
 import com.example.smartorder.model.KakaoApi;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
 import java.net.URI;
-import java.net.URL;
-import java.net.URLEncoder;
 import java.nio.charset.Charset;
-import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import lombok.RequiredArgsConstructor;
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
-import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.util.UriComponents;
 import org.springframework.web.util.UriComponentsBuilder;
 
 @Component
 @RequiredArgsConstructor
 public class LocationComponents {
 
-//	@Value("${kakao.key}")
-	private String apiKey = "5639e87e58d677766b2f3146fd14828b";
+	@Value("${kakao.key}")
+	private String apiKey;
 
 	public KakaoApi.Documents getKakaoApiDouments(String address) {
 		List<KakaoApi.Documents> documents =
